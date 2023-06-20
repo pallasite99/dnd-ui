e760fbeeb204b79111d6bc6841055efb3817509d
Salil Apte <salil.apte99@gmail.com>
2023-06-20 07:55:01 +0530

    skeletal login and homepage flow

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css'; // Import the CSS file
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-background">
+        <div className="home-content">
+          <h2>Welcome to the Dungeons and Dragons game!</h2>
+          {/* Add your game content here */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.css'; // Import the CSS file
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // Perform login logic, e.g., make a request to a backend server
+
+    // Redirect to the home page after successful login
+    navigate('/home');
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
